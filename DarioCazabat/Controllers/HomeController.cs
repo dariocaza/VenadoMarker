c4f7f45c3f61a166510a916dfa19633f77987e0c
dariocaza <dario.cazabat@gmail.com>
2017-06-23 11:59:31 -0300

    se agrego el form en contacto

--- DarioCazabat/Controllers/HomeController.cs
+++ DarioCazabat/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
 
 namespace DarioCazabat.Controllers
 {
@@ -30,5 +32,39 @@ namespace DarioCazabat.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contacto(string nombre, string email, string consulta)
+        {
+            SmtpClient clienteSmtp = new SmtpClient("smtp.gmail.com", 587);
+            //clienteSmtp.Host = "smtp.gmail.com";
+            //clienteSmtp.Port = 587;
+            clienteSmtp.Credentials = new NetworkCredential("dario.cazabat@gmail.com", "rivermivida14*-");
+            clienteSmtp.EnableSsl = true;
+
+            //mail para el dueño de la aplicación
+            MailMessage mensajeParaAplicacion = new MailMessage();
+            mensajeParaAplicacion.From = new MailAddress("dario.cazabat@gmail.com", "Mensaje desde Venado MARKER");
+            mensajeParaAplicacion.To.Add("dario.cazabat@gmail.com");
+            mensajeParaAplicacion.Subject = "Mensaje desde Venado MARKER";
+            mensajeParaAplicacion.Body = nombre + " (" + email + ") te contactó desde la aplicación Venado MARKER y dejó la siguiente consulta: " + consulta;
+
+            clienteSmtp.Send(mensajeParaAplicacion);
+
+            //mail para el usuario
+            MailMessage mensajeParaUsuario = new MailMessage();
+            mensajeParaUsuario.From = new MailAddress("dario.cazabat@gmail.com", "Tu mensaje fue enviado");
+            mensajeParaUsuario.To.Add(email);
+            mensajeParaUsuario.Subject = "Gracias por comunicarte con Venado MARKER!";
+            mensajeParaUsuario.IsBodyHtml = true;
+            mensajeParaUsuario.Body = "Hola " + nombre + ", <br>Gracias por contactarte con nosotros!<br>Tu mensaje fue: <b>" + consulta + "</b><br>Saludos!!!!<br><b>El equipo del blog</b><br><img src=\"http://statictycf5b.tycsports.com/sites/default/files/styles/landscape_642_366/public/nota_periodistica/16-02-2016_buenos_aires_mariano_pavone_festeja.jpg\">";
+
+            clienteSmtp.Send(mensajeParaUsuario);
+
+            //return RedirectToAction("Index");
+            ViewBag.Nombre = nombre;
+            return View();
+        }
+
     }
 }
